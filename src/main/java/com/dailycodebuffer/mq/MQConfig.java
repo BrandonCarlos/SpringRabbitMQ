280423428318285a671ca33ee983067fc40e082b
Brandon Carlos <brandon_marzo@hotmail.com>
2024-09-04 11:54:22 -0300

    Created Message Converter method to convert from determined archive to Json archive

--- src/main/java/com/dailycodebuffer/mq/MQConfig.java
+++ src/main/java/com/dailycodebuffer/mq/MQConfig.java
@@ -4,8 +4,10 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MessageConverter;
 
 @Configuration
 public class MQConfig {
@@ -27,6 +29,11 @@ public class MQConfig {
     @Bean
     public Binding binding(Queue queue, TopicExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(RoutingKey);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return (MessageConverter) new Jackson2JsonMessageConverter();
     }
 
 
