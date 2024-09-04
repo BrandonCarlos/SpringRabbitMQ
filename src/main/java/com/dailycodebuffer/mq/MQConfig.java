f069c1ff253a6c601cf92fc8b417fc837bdb6232
Brandon Carlos <brandon_marzo@hotmail.com>
2024-09-04 11:50:57 -0300

    Created binding between Queue, TopicExchange and I was created the RourtingKey as well

--- src/main/java/com/dailycodebuffer/mq/MQConfig.java
+++ src/main/java/com/dailycodebuffer/mq/MQConfig.java
@@ -1,5 +1,7 @@
 package com.dailycodebuffer.mq;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +12,7 @@ public class MQConfig {
 
     private static final String QUEUE = "message_queue";
     private static final String EXCHANGE = "message_exchange";
+    private static final String RoutingKey = "message_routingKey";
 
     @Bean
     public Queue queue(){
@@ -20,5 +23,11 @@ public class MQConfig {
     public TopicExchange exchange() {
         return new TopicExchange(EXCHANGE);
     }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(RoutingKey);
+    }
+
 
 }
